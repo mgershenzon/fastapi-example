461f47236f432b822afdd7873c1a1403cf4c1938
michal.gershenzon <michal.gershenzon@refinitiv.com>
2022-07-12 17:56:43 +0300

    Add link to /docs from /

--- fastapi_example/api/routes/root.py
+++ fastapi_example/api/routes/root.py
@@ -1,14 +1,14 @@
-from fastapi import APIRouter
-from fastapi.responses import PlainTextResponse
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
 
 router = APIRouter()
 
 
-@router.get('/', response_class=PlainTextResponse)
-async def default_endpoint():
+@router.get('/', response_class=HTMLResponse)
+async def default_endpoint(request: Request):
     """
     Return 'OK!' response of the service is alive
 
     :return: 'OK!' string
     """
-    return 'Hi! This is the root level. Try calling `/docs` endpoint :)'
+    return 'Hi! This is the root level. Try <a href="docs"> %sdocs </a> endpoint :)' % request.url
