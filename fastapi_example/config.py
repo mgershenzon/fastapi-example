2b74faa02221f86a28316a8599c9fe5cad87dd82
michal.gershenzon <michal.gershenzon@refinitiv.com>
2024-01-29 12:42:12 +0200

    Add pydantic-settings to config

--- fastapi_example/config.py
+++ fastapi_example/config.py
@@ -1,14 +1,29 @@
 import os
+from typing import Annotated
+
+from pydantic import AfterValidator, DirectoryPath, FilePath
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Config:
-    HOST = os.getenv('HOST', '0.0.0.0')
-    PORT = os.getenv('PORT', 80)
-    WORKERS = os.getenv('WORKERS', 2)
-    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-    LOGGING_CONFIG = os.path.join(ROOT_DIR, '', 'logging', 'conf.yaml')
-    DISPLAY_TRACEBACK_ON_500: bool = os.getenv("DISPLAY_TRACEBACK_ON_500", True)
-    ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "")
-    PROFILING_ON = os.getenv("PROFILING_ON", False)
-    VERSION = "1.0"
-    TITLE = "Fastapi Example"
+class ConfigModel(BaseSettings):
+
+    # model_config allows to use env vars or ".env" file to override the attributes below in ConfigModel
+    # env vars take priority over env files. If none exists, it will use the defaults
+    model_config = SettingsConfigDict(
+        env_prefix='',  # you can set a prefix to avoid collision or mistakes with the environment variables
+        env_file=('.env', '.env.prod')  # `.env.prod` takes priority over `.env`
+    )
+
+    HOST: str = '0.0.0.0'
+    PORT: int = 80
+    WORKERS: int = 2
+    ROOT_DIR: Annotated[DirectoryPath, AfterValidator(str)] = os.path.dirname(os.path.abspath(__file__))
+    LOGGING_CONFIG: Annotated[FilePath, AfterValidator(str)] = os.path.join(ROOT_DIR, '', 'logging', 'conf.yaml')
+    DISPLAY_TRACEBACK_ON_500: bool = True
+    ALLOWED_HOSTS: str = ""
+    PROFILING_ON: bool = False
+    VERSION: str = "1.0"
+    TITLE: str = "Fastapi Example"
+
+
+Config = ConfigModel()
