eb2ded722090db6bec49e3a771a1760645055aab
michal.gershenzon <michal.gershenzon@refinitiv.com>
2024-02-07 12:37:11 +0200

    Remove the usage of loguru and use standard library instead

--- fastapi_example/core/events.py
+++ fastapi_example/core/events.py
@@ -2,23 +2,31 @@ import logging
 from typing import Callable
 
 from fastapi import FastAPI
-from loguru import logger as loguru_logger
 
 logger = logging.getLogger(__name__)
 
 
 def create_start_app_handler(app: FastAPI) -> Callable:  # type: ignore
     async def start_app() -> None:
-        # In some apps DB connections are opened here
-        logger.info(f'Start app handler (This will be printed once per worker). Code can be found in file {__file__}')
+        try:
+            # In some apps DB connections are opened here
+            logger.info(
+                f'Start app handler (This will be printed once per worker). Code can be found in file {__file__}'
+            )
+        except Exception as e:
+            logger.error(f"Error in start_app: {e}")
 
     return start_app
 
 
 def create_stop_app_handler(app: FastAPI) -> Callable:  # type: ignore
-    @loguru_logger.catch
     async def stop_app() -> None:
-        # In some apps DB connections are closed here
-        logger.info(f'Stop app handler (This will be printed once per worker). Code can be found in file {__file__}')
+        try:
+            # In some apps DB connections are closed here
+            logger.info(
+                f'Stop app handler (This will be printed once per worker). Code can be found in file {__file__}'
+            )
+        except Exception as e:
+            logger.error(f"Error in stop_app: {e}")
 
     return stop_app
--- tests/test_core/test_events.py
+++ tests/test_core/test_events.py
@@ -1,8 +1,11 @@
+import asyncio
 from unittest.mock import MagicMock
 
+from fastapi import FastAPI
 from starlette.testclient import TestClient
 
 from fastapi_example.core import events
+from fastapi_example.core.events import create_start_app_handler, create_stop_app_handler
 from fastapi_example.main import app
 
 
@@ -15,3 +18,60 @@ def test_create_stop_start_app_handlers():
         assert info_mock.call_count == 1
 
     assert info_mock.call_count == 2
+
+
+def test_start_app(mocker):
+    # Create a mock FastAPI instance
+    application = FastAPI()
+
+    # Patch the logger in the module
+    mock_logger = mocker.patch('fastapi_example.core.events.logger')
+
+    # Call the create_start_app_handler function to get the start_app handler
+    start_app = create_start_app_handler(application)
+
+    # Call the start_app handler explicitly
+    asyncio.run(start_app())
+
+    # Assert that logger.info was called
+    mock_logger.info.assert_called_once()
+
+
+def test_start_app_exception(mocker):
+    # Create a mock FastAPI instance
+    application = FastAPI()
+
+    # Patch the logger in the module
+    mock_logger = mocker.patch('fastapi_example.core.events.logger')
+
+    # Set the mock logger to raises an exception when info is called
+    mock_logger.info.side_effect = Exception("Test exception")
+
+    # Call the create_start_app_handler function to get the start_app handler
+    start_app = create_start_app_handler(application)
+
+    # Call the start_app handler explicitly
+    asyncio.run(start_app())
+
+    # Assert that logger.error was called
+    mock_logger.error.assert_called_once_with("Error in start_app: Test exception")
+
+
+def test_stop_app_exception(mocker):
+    # Create a mock FastAPI instance
+    application = FastAPI()
+
+    # Patch the logger in the module
+    mock_logger = mocker.patch('fastapi_example.core.events.logger')
+
+    # Set the mock logger to raises an exception when info is called
+    mock_logger.info.side_effect = Exception("Test exception")
+
+    # Call the create_start_app_handler function to get the stop_app handler
+    stop_app = create_stop_app_handler(application)
+
+    # Call the stop_app handler explicitly
+    asyncio.run(stop_app())
+
+    # Assert that logger.error was called
+    mock_logger.error.assert_called_once_with("Error in stop_app: Test exception")
